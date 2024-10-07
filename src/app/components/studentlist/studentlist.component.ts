d55e0ecceae0b40bba3665373f997b67a2b9f427
jennyfaye <jennyfaye.araiz@gmail.com>
2024-10-07 21:09:25 +0800

    feat: add studentlist services

--- src/app/components/studentlist/studentlist.component.ts
+++ src/app/components/studentlist/studentlist.component.ts
@@ -1,15 +1,36 @@
-import { Component } from '@angular/core';
+import { StudentlistService } from './../../services/studentlist.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-studentlist',
   templateUrl: './studentlist.component.html',
   styleUrl: './studentlist.component.css'
 })
-export class StudentlistComponent {
-  nameList: string[] = ["Jenny", "Angeline"];
-  name: string = '';
+export class StudentlistComponent implements OnInit{
+  studentList: string[] = [];
+  student: string = '';
 
-  addStudent() {
-    this.nameList.push(this.name);
+  constructor(private studentlistService: StudentlistService){
+  }
+
+  ngOnInit(): void {
+    this.studentList = this.studentlistService.getStudent();
+  }
+
+  addStudent(): void {
+    this.studentlistService.addStudent(this.student);
+    this.student = '';
+  }
+
+  updateStudent(index: number): void {
+    const currentName = this.studentList[index];
+    const newName = prompt('Enter new name for the student:', currentName);
+    if (newName !== null && newName.trim() !== '') {
+      this.studentlistService.updateStudent(index, newName.trim());
+    }
+  }
+
+  deleteStudent(index: number): void {
+    this.studentlistService.deleteStudent(index);
   }
 }
